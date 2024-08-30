bdbfa5befab4f74aed0cad9d379823ef4504d80c
saba-ramezani <saba.rad.sr@gmail.com>
2024-08-31 01:13:24 +0330

    implemented feature component with no css

--- src/components/feature/Feature.js
+++ src/components/feature/Feature.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import "./feature.css";
 
-const Feature = () => {
+const Feature = ({title, text}) => {
   return (
-    <div>Feature</div>
+    <div className='gpt3__features-container__feature'>
+      <div className='gpt3__features-container__feature-title'>
+          <div></div>
+          <h1>{title}</h1>
+      </div>
+      <div className='gpt3__features-container__feature-text'>
+        {text}
+      </div>
+    </div>
   )
 }
 
--- src/containers/whatGPT3/WhatGPT3.js
+++ src/containers/whatGPT3/WhatGPT3.js
@@ -6,16 +6,16 @@ const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
       <div className='gpt3__whatgpt3-feature'>
-        <Feature />
+        <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
       </div>
       <div className='gpt3__whatgpt3-heading'>
         <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
         <button type='Button'>Explore The Library</button>
       </div>
       <div className='gpt3__whatgpt3-container'>
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. " />
+        <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+        <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
       </div>
     </div>
   )
