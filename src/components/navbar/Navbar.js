6d1daa59c2c049b91e911fb40ac560cd3496bb62
saba-ramezani <saba.rad.sr@gmail.com>
2024-08-30 15:41:52 +0330

    added navbar links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,6 +10,13 @@ const Navbar = () => {
         <div className='gpt3__navbar-links_logo'>
           <img src={logo} alt="logo" />
         </div>
+        <div className='gpt3__navbar-links_container'>
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Ctudies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
       </div>
 
     </div>
