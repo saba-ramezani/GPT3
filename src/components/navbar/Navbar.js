174d062619d39a223cfe6c0d46534b98714ca0bc
saba-ramezani <saba.rad.sr@gmail.com>
2024-08-30 15:33:24 +0330

    added gpt3 logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import "./navbar.css";
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+
+    </div>
   )
 }
 
