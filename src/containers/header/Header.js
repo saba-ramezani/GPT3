8fc3fdb8c5fa4b357d81433c00a4f35a02a090bd
saba-ramezani <saba.rad.sr@gmail.com>
2024-08-30 23:32:44 +0330

    added header components

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import "./header.css";
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id="home">
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Let's Build Something Amazing with GPT-3 OpenAI</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <div className='gpt3__header-content__input'>
+          <input type="email" placeholder='Your Email Address' />
+          <button type='button'>Get Started</button>
+        </div>
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt="People" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+        <div className='gpt3__header-image'>
+          <img src={ai} alt="AI" />
+        </div>
+      </div> 
+    </div>
   )
 }
 
-export default Header
+export default Header;
