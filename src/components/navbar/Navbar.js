83535a81c01bea32c1a204f2662746512a3fb23e
saba-ramezani <saba.rad.sr@gmail.com>
2024-08-30 16:12:49 +0330

    added and styled menu elements in navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./navbar.css";
 import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 
+
+const Menu = () => {
+  <>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#wgpt3">What is GPT3?</a></p>
+    <p><a href="#possibility">Open AI</a></p>
+    <p><a href="#features">Case Ctudies</a></p>
+    <p><a href="#blog">Library</a></p>
+  </>
+}
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
@@ -21,6 +33,27 @@ const Navbar = () => {
       <div className='gpt3__navbar-sign'>
         <button className='gpt3__navbar-sign_signInBtn' type='button' onClick={() => {console.log('Sign in pressed!')}}>Sign In</button>
         <button className='gpt3__navbar-sign_signUpBtn' type='button' onClick={() => {console.log('Sign up pressed!')}}>Sign Up</button>
+      </div>
+      <div className='gpt3__navbar-menu'>
+        {toggleMenu
+          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)}/> 
+          : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)}/> 
+        }
+        {toggleMenu && (
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+            <div className='gpt3__navbar-menu_container-links'>
+              <p><a href="#home">Home</a></p>
+              <p><a href="#wgpt3">What is GPT3?</a></p>
+              <p><a href="#possibility">Open AI</a></p>
+              <p><a href="#features">Case Ctudies</a></p>
+              <p><a href="#blog">Library</a></p>
+            </div>
+            <div className='gpt3__navbar-menu_container-links-sign'>
+              <button className='gpt3__navbar-menu_container-links-sign_signInBtn' type='button' onClick={() => {console.log('Sign in pressed!')}}>Sign In</button>
+              <button className='gpt3__navbar-menu_container-links-sign_signUpBtn' type='button' onClick={() => {console.log('Sign up pressed!')}}>Sign Up</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
