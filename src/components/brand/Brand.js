f9dfeb1f565e2c1132098c7d63cae522ecfda50c
saba-ramezani <saba.rad.sr@gmail.com>
2024-08-31 00:31:42 +0330

    added brands components and stylings

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import "./brand.css";
+import {google, slack, atlassian, dropbox, shopify} from "./imports";
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt="Google" />
+      </div>
+      <div>
+        <img src={slack} alt="Slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="Atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="Dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="Shopify" />
+      </div>
+    </div>
   )
 }
 
