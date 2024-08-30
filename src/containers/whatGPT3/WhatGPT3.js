511304f6e8f39c18fe966449aaccf2018a6c5d56
saba-ramezani <saba.rad.sr@gmail.com>
2024-08-31 00:39:33 +0330

    added initial WhatGPT3 components with no css

--- src/containers/whatGPT3/WhatGPT3.js
+++ src/containers/whatGPT3/WhatGPT3.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import "./whatGPT3.css";
+import { Feature } from '../../components';
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient-text'></h1>
+        <p>Explre The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 
