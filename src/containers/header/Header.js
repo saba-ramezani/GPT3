f1889951e3b1474069e454cec8ff96124c39f588
saba-ramezani <saba.rad.sr@gmail.com>
2024-08-31 00:09:54 +0330

    added css styling for different devices with different widths for header

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -17,10 +17,10 @@ const Header = () => {
           <img src={people} alt="People" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        {/*<div className='gpt3__header-image'>
-          <img src={ai} alt="AI" />
-        </div>*/}
       </div> 
+      <div className='gpt3__header-image'>
+          <img src={ai} alt="AI" />
+        </div>
     </div>
   )
 }
