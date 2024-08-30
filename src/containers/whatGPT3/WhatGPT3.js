379df71a24f95fb8c5e0291f74e8e26ebb962f74
saba-ramezani <saba.rad.sr@gmail.com>
2024-08-31 01:07:28 +0330

    added styling for whatGPT3 section

--- src/containers/whatGPT3/WhatGPT3.js
+++ src/containers/whatGPT3/WhatGPT3.js
@@ -9,8 +9,8 @@ const WhatGPT3 = () => {
         <Feature />
       </div>
       <div className='gpt3__whatgpt3-heading'>
-        <h1 className='gradient-text'></h1>
-        <p>Explre The Library</p>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <button type='Button'>Explore The Library</button>
       </div>
       <div className='gpt3__whatgpt3-container'>
         <Feature />
