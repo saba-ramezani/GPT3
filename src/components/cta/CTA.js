4fcfd2566f43a7118c6a8fc6af5ed15326eb3e57
saba-ramezani <saba.rad.sr@gmail.com>
2024-08-31 12:56:24 +0330

    added and styled cta section

--- src/components/cta/CTA.js
+++ src/components/cta/CTA.js
@@ -3,7 +3,15 @@ import "./cta.css";
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='gpt3__cta'>
+      <div className='gpt3__cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className='gpt3__cta-button'>
+        <button type='Button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
