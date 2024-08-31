ddc28450e17f9c37037d6005a1758aac89f55092
saba-ramezani <saba.rad.sr@gmail.com>
2024-08-31 13:02:41 +0330

    added initial design of blog section with no styling

--- src/containers/blog/Blog.js
+++ src/containers/blog/Blog.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import "./blog.css";
+import { Article } from '../../components';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, 
+        We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article />
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+        </div>
+      </div>
+    </div>
   )
 }
 
