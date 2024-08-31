0e34cbacf3ceb35ce52a7cf0c35d9926061d5056
saba-ramezani <saba.rad.sr@gmail.com>
2024-08-31 12:21:58 +0330

    added and styled possibility section

--- src/containers/possibility/Possibility.js
+++ src/containers/possibility/Possibility.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import "./possibility.css";
+import possibilityImage from "../../assets/possibility.png";
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='gpt3__possibility section__padding'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <button type='Button'>Request Early Access to Get Started</button>
+      </div>
+    </div>
   )
 }
 
