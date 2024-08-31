34feed88e322fd7284d374b0ef4950042374bd27
saba-ramezani <saba.rad.sr@gmail.com>
2024-08-31 15:25:14 +0330

    fixed the bug with possibility section id

--- src/containers/possibility/Possibility.js
+++ src/containers/possibility/Possibility.js
@@ -4,7 +4,7 @@ import possibilityImage from "../../assets/possibility.png";
 
 const Possibility = () => {
   return (
-    <div className='gpt3__possibility section__padding'>
+    <div className='gpt3__possibility section__padding' id='possibility'>
       <div className='gpt3__possibility-image'>
         <img src={possibilityImage} alt="possibility" />
       </div>
